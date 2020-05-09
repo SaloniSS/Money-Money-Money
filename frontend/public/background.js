4c8e6e14deb1609f6cc6265a1ffbe0e523a999fe
SaloniSS <sxs170015@utdallas.edu>
2020-05-09 16:56:00 -0500

    Find product id

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -2,6 +2,13 @@ const testUrls = ["amazon.com/gp/product", "url2.com"];
 
 chrome.tabs.onUpdated.addListener(function (tabID, changeInfo, tab) {
   if (changeInfo.url) {
+    url = tab.url;
+    index = url.indexOf("/dp/");
     alert(tab.url);
+    if (index == -1){
+      alert("Not a product page");
+    } else {
+      alert(url.substring(index+4, index+13))
+    }
   }
 });
