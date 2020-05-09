2f35e50f165e290f2ba8fb4176471690adad0f2a
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-05-09 14:35:50 -0500

    buttons setup

--- frontend/src/Button.js
+++ frontend/src/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Icon } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 import "./Button.css";
 
 const ModelButton = (props) => {
@@ -7,7 +7,7 @@ const ModelButton = (props) => {
     <Button animated="vertical" size="huge">
       <Button.Content hidden>{props.text}</Button.Content>
       <Button.Content visible>
-        <Icon name={props.icon} />
+        <img className="thumbnail" src={props.icon} />
       </Button.Content>
     </Button>
   );
--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -3,7 +3,24 @@ import ModelButton from "./Button";
 import logo from "./logo.svg";
 import "./Content.css";
 
-const MODES = ["chipotle", "boba", "chicken nuggies", "toilet paper"];
+const MODES = [
+  {
+    name: "chipotle",
+    icon: "https://webstockreview.net/images/green-clipart-milkshake-9.png",
+  },
+  {
+    name: "boba",
+    icon: "https://webstockreview.net/images/green-clipart-milkshake-9.png",
+  },
+  {
+    name: "chicken nuggies",
+    icon: "https://webstockreview.net/images/green-clipart-milkshake-9.png",
+  },
+  {
+    name: "toilet paper",
+    icon: "https://webstockreview.net/images/green-clipart-milkshake-9.png",
+  },
+];
 
 const Content = () => {
   const [mode, setMode] = useState(0);
@@ -13,8 +30,9 @@ const Content = () => {
       <p>Current Mode:</p>
       <img src={logo} className="App-logo" alt="mode" />
       <div className="modelSelection">
-        <ModelButton text="hello" icon="shop" />
-        <ModelButton text="chipotle burritos" icon="shop" />
+        {MODES.map((mode) => (
+          <ModelButton text={mode.name} icon={mode.icon} />
+        ))}
       </div>
     </div>
   );
