b9b7f3b19d0aa703cbd62fe57a00074a51c59c76
SaloniSS <sxs170015@utdallas.edu>
2020-05-09 16:56:34 -0500

    Change in url

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -1,4 +1,4 @@
-const testUrls = ["amazon.com/gp/product", "url2.com"];
+const testUrls = ["amazon.com/dp", "url2.com"];
 
 chrome.tabs.onUpdated.addListener(function (tabID, changeInfo, tab) {
   if (changeInfo.url) {
