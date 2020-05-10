7b76d1ba0a9c214916cf4f3ebcb13f830729b3eb
SaloniSS <sxs170015@utdallas.edu>
2020-05-10 02:00:33 -0500

    Cart popups

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -3,12 +3,20 @@ const testUrls = ["amazon.com/dp", "url2.com"];
 chrome.tabs.onUpdated.addListener(function (tabID, changeInfo, tab) {
   if (changeInfo.url) {
     url = tab.url;
-    index = url.indexOf("/dp/");
-    alert(tab.url);
-    if (index == -1) {
-      alert("Not a product page");
-    } else {
-      alert(url.substring(index + 4, index + 13));
+    cartIndex = url.indexOf("/gp/");
+    if (cartIndex != -1) {
+      var q1 = confirm("Are you sure you want this?");
+      if (q1 == true) {
+        console.log("q1 passed");
+        var q2 = confirm("Do you really need this?");
+        if (q2 == true) {
+          console.log("q2 passed");
+          var q3 = confirm("Are you actually going to use this?");
+          if (q3 == true) {
+            console.log("q3 passed");
+          }
+        }
+      }
     }
   }
 });
