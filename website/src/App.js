d3046ca71c452c0f5e874b1882549939c1345266
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-05-09 23:52:32 -0500

    add content

--- website/src/App.js
+++ website/src/App.js
@@ -7,6 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Money Money Money... Always Funny</h1>
+        <p>
+          Do you want a convenient way to save money? Look no further! Here is
+          the extension of the future, Money Money Money! It must be funny in
+          the rich man's world... Since we all aren't rich how about you compare
+          uncessary purchases to things you really want?
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <iframe
           width="560"
