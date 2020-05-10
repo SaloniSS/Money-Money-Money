05365919215967ed274a0e68f6f3d0c424809c66
SaloniSS <sxs170015@utdallas.edu>
2020-05-10 02:01:14 -0500

    Conditional rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+/* global chrome */
+
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import "./semantic-ui.css";
 import Content from "./Content";
 import AlternatePrice from "./AlternatePrice";
 
 function App() {
+
+  const [productPage, setProductPage] = useState(false)
+
+  useEffect(() => {
+    //get product id
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        const url = tabs[0].url;
+        const index = url.indexOf("/dp/");
+        if (index == -1){
+          //content = content
+          setProductPage(false);
+        } else{
+          //content = alternateprice
+          setProductPage(true);
+        }
+    });
+  });
+
   return (
     <div className="App">
       <header className="header">
         <h2>MoneyMoneyMoney</h2>
       </header>
-      <AlternatePrice />
+      {(productPage && <AlternatePrice/>) || <Content/>}
     </div>
   );
 }
