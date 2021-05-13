462d04834334a6da31b5a135b0e93bbf1d55f2c5
Megan Tran <meganjtran@gmail.com>
2021-05-12 19:15:48 -0600

    very important changes being made

--- website/src/App.js
+++ website/src/App.js
@@ -10,9 +10,9 @@ function App() {
         <h1>Money Money Money... Always Funny</h1>
         <p>
           Do you want a convenient way to save money? Look no further! Here is
-          the extension of the future, Money Money Money! It must be funny in
-          the rich man's world... Since we all aren't rich how about you compare
-          uncessary purchases to things you really want?
+          the extension of the future, MoneyMoneyMoney! Must be funny, in a rich
+          man's world... Since we all aren't rich how about you compare
+          unnecessary purchases to things you really want?
         </p>
         <img src={logo} className="App-logo" alt="logo" />
         <iframe
@@ -49,21 +49,33 @@ function App() {
         ></iframe>
         <div className="button-area">
           <Button className="button" variant="contained">
-            <a href="https://chrome.google.com/webstore/detail/money-money-money/ehdcenmhmjlkmlnmlglncndglmoglojd" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://chrome.google.com/webstore/detail/money-money-money/ehdcenmhmjlkmlnmlglncndglmoglojd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Try It
             </a>
           </Button>
         </div>
         <div className="button-area">
           <Button className="button" variant="contained">
-            <a href="https://devpost.com/software/moneymoneymoney" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://devpost.com/software/moneymoneymoney"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               More Info
             </a>
           </Button>
         </div>
         <div className="button-area">
           <Button className="button" variant="contained">
-            <a href="https://github.com/SaloniSS/Money-Money-Money" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/SaloniSS/Money-Money-Money"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Source Code
             </a>
           </Button>
@@ -74,7 +86,11 @@ function App() {
         </p>
         <div className="button-area">
           <Button className="button" variant="contained">
-            <a href="mailto:hackathon.dream.team.utd@gmail.com" target="_blank" rel="noopener noreferrer">
+            <a
+              href="mailto:hackathon.dream.team.utd@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Contact the Developers
             </a>
           </Button>
